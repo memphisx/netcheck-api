498767620a09bb7d18f7ede3f64e883fba55a7b5
Kyriakos Bompotis <memphisx@gmail.com>
2020-06-09 12:54:17 +0100

    issue-1: Database config. Move to yaml spring config. Enable JPA

--- src/main/java/com/bompotis/netcheck/NetcheckApplication.java
+++ src/main/java/com/bompotis/netcheck/NetcheckApplication.java
@@ -2,8 +2,12 @@ package com.bompotis.netcheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableScheduling
 public class NetcheckApplication {
 
 	public static void main(String[] args) {
