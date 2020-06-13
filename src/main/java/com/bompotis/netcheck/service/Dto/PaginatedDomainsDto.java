a42f85812f4cce7662c8a8c9b9a12cad9e0441c1
Kyriakos Bompotis <memphisx@gmail.com>
2020-06-13 14:31:17 +0100

    issue-19: Pagination and other fixes.
    - Fixed wrong db structure defined by JPA
    - Minor Refactoring of Class names and structure
    - Added missing Dto.
    - Initial support for paginated results in endpoints that return lists
    - Added page metadata on the above endpoints
    - Added HATEOAS links for next/previous page on the above endpoints

---
+++ src/main/java/com/bompotis/netcheck/service/Dto/PaginatedDomainsDto.java
@@ -0,0 +1,24 @@
+package com.bompotis.netcheck.service.Dto;
+
+import java.util.List;
+
+/**
+ * Created by Kyriakos Bompotis on 12/6/20.
+ */
+public class PaginatedDomainsDto extends PaginatedDto{
+    private final List<String> domains;
+
+    public PaginatedDomainsDto(List<String> domains,
+                               long totalElements,
+                               int totalPages,
+                               int number,
+                               int size) {
+        super(totalElements, totalPages, number, size);
+        this.domains = domains;
+    }
+
+    public List<String> getDomains() {
+        return domains;
+    }
+
+}
---
+++ src/main/java/com/bompotis/netcheck/service/Dto/PaginatedDto.java
@@ -0,0 +1,40 @@
+package com.bompotis.netcheck.service.Dto;
+
+import java.util.List;
+
+/**
+ * Created by Kyriakos Bompotis on 12/6/20.
+ */
+public class PaginatedDto {
+
+    private final long totalElements;
+    private final int totalPages;
+    private final int number;
+    private final int size;
+
+    public PaginatedDto(long totalElements,
+                        int totalPages,
+                        int number,
+                        int size) {
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.number = number;
+        this.size = size;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
