901b02e8f6d9fad4ca61f17864173c438bf5ac85
Kyriakos Bompotis <memphisx@gmail.com>
2020-06-24 13:23:41 +0100

    Remove unnecessary imports

--- src/main/java/com/bompotis/netcheck/data/entity/AbstractTimestampablePersistable.java
+++ src/main/java/com/bompotis/netcheck/data/entity/AbstractTimestampablePersistable.java
@@ -5,7 +5,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.domain.Persistable;
-import org.springframework.util.ClassUtils;
 
 import javax.persistence.*;
 import java.io.Serializable;
