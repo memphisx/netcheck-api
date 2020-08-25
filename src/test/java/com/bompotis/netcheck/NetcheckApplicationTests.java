149c28510d572e8b20a683bb7a415fccf638a73a
Kyriakos Bompotis <memphisx@gmail.com>
2020-08-25 12:47:53 +0100

    Configs test

--- src/test/java/com/bompotis/netcheck/NetcheckApplicationTests.java
+++ src/test/java/com/bompotis/netcheck/NetcheckApplicationTests.java
@@ -1,13 +1,25 @@
 package com.bompotis.netcheck;
 
+import com.bompotis.netcheck.scheduler.batch.notification.config.PushoverConfig;
+import com.bompotis.netcheck.scheduler.batch.notification.config.WebhookConfig;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NetcheckApplicationTests {
 
+	@Autowired
+	private WebhookConfig webhookConfig;
+
+	@Autowired
+	private PushoverConfig pushoverConfig;
+
 	@Test
 	void contextLoads() {
+		Assert.assertFalse(webhookConfig.getEnabled());
+		Assert.assertFalse(pushoverConfig.getEnabled());
 	}
 
 }
