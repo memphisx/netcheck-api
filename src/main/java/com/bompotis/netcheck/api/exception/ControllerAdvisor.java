8147ffad3acbf6e4a18b7c02ef84a16c15c2c3fc
Kyriakos Bompotis <memphisx@gmail.com>
2020-09-24 18:11:25 +0100

    Added Controller Advisor and Exceptions for handling error responses

--- src/main/java/com/bompotis/netcheck/api/exception/ControllerAdvisor.java
+++ src/main/java/com/bompotis/netcheck/api/exception/ControllerAdvisor.java
@@ -1,5 +1,24 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.bompotis.netcheck.api.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,8 +29,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,15 +42,41 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(ControllerAdvisor.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> handleCityNotFoundException(
+    public ResponseEntity<Object> handleIllegalArgumentException(
             IllegalArgumentException ex, WebRequest request) {
 
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
+        body.put("timestamp", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
         body.put("message", ex.getMessage());
 
+
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidAuthHeaders.class)
+    public ResponseEntity<Object> handleInvalidAuthHeaders() {
+        return handleUnauthorizedStatusException();
+    }
+
+    @ExceptionHandler(UnauthorizedStatusException.class)
+    public ResponseEntity<Object> handleUnauthorizedStatusException() {
+
+        var headers = new HttpHeaders();
+        headers.add(HttpHeaders.WWW_AUTHENTICATE,"Basic realm=\"Server Realm\"");
+
+        return new ResponseEntity<>(null, headers, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 
     @Override
@@ -40,7 +85,7 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
             HttpStatus status, WebRequest request) {
 
         Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDate.now());
+        body.put("timestamp", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
         body.put("status", status.value());
 
         List<String> errors = ex.getBindingResult()
