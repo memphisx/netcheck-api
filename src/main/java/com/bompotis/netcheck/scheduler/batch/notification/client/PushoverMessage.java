064a1c4606eddcf937011f138c11e2656bac27be
Kyriakos Bompotis <memphisx@gmail.com>
2023-08-09 21:38:11 +0100

    issue-368: Custom pushover implementation replacing pushover4j (#369)

---
+++ src/main/java/com/bompotis/netcheck/scheduler/batch/notification/client/PushoverMessage.java
@@ -0,0 +1,167 @@
+package com.bompotis.netcheck.scheduler.batch.notification.client;
+
+public class PushoverMessage {
+
+    private String apiToken;
+    private String userId;
+    private String message;
+    private String htmlMessage;
+    private String device;
+    private String title;
+    private String url;
+    private String titleForURL;
+    private MessagePriority priority = MessagePriority.NORMAL;
+    private Long timestamp;
+    private String sound;
+    private PushoverMessage() {}
+    public static Builder builderWithApiToken(String token) {
+        return new Builder().setApiToken(token);
+    }
+    public static class Builder {
+        private final PushoverMessage msg;
+
+        public Builder() {
+            msg = new PushoverMessage();
+        }
+
+        public PushoverMessage build() {
+            return msg;
+        }
+
+        /**
+         * (required) - your application's API token
+         */
+        public Builder setApiToken(String apiToken) {
+            msg.apiToken = apiToken;
+            return this;
+        }
+
+        /**
+         * (required) - the user key (not e-mail address) of your user (or you),
+         * viewable when logged into the dashboard
+         */
+        public Builder setUserId(String userId) {
+            msg.userId = userId;
+            return this;
+        }
+
+        /**
+         * (required) - your message
+         */
+        public Builder setMessage(String message) {
+            msg.message = message;
+            return this;
+        }
+
+        public Builder setHtmlMessage(String htmlMessage) {
+            msg.htmlMessage = htmlMessage;
+            return this;
+        }
+
+        /**
+         * (optional) - your user's device identifier to send the message
+         * directly to that device, rather than all of the user's devices
+         */
+        public Builder setDevice(String device) {
+            msg.device = device;
+            return this;
+        }
+
+        /**
+         * (optional) - your message's title, otherwise uses your app's name
+         */
+        public Builder setTitle(String title) {
+            msg.title = title;
+            return this;
+        }
+
+        /**
+         * (optional) - a supplementary URL to show with your message
+         */
+        public Builder setUrl(String url) {
+            msg.url = url;
+            return this;
+        }
+
+        /**
+         * (optional) - a title for your supplementary URL
+         */
+        public Builder setTitleForURL(String titleForURL) {
+            msg.titleForURL = titleForURL;
+            return this;
+        }
+
+        /**
+         * (optional) - set to MessagePriority.HIGH to display as high-priority
+         * and bypass quiet hours, or MessagePriority.QUIET to always send as a
+         * quiet notification
+         */
+        public Builder setPriority(MessagePriority priority) {
+            msg.priority = priority;
+            return this;
+        }
+
+        /**
+         * (optional) - set to a Unix timestamp to have your message show with a
+         * particular time, rather than now
+         */
+        public Builder setTimestamp(Long timestamp) {
+            msg.timestamp = timestamp;
+            return this;
+        }
+
+        /**
+         * (optional) - set to the name of one of the sounds supported by device
+         * clients to override the user's default sound choice
+         */
+        public Builder setSound(String sound) {
+            msg.sound = sound;
+            return this;
+        }
+    }
+
+    public String getApiToken() {
+        return apiToken;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitleForURL() {
+        return titleForURL;
+    }
+
+    public MessagePriority getPriority() {
+        return priority;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public String getHtmlMessage() {
+        return htmlMessage;
+    }
+
+}
---
+++ src/main/java/com/bompotis/netcheck/scheduler/batch/notification/client/PushoverRestClient.java
@@ -0,0 +1,87 @@
+package com.bompotis.netcheck.scheduler.batch.notification.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public class PushoverRestClient {
+
+    private record PushoverErrorResponse(HttpStatusCode statusCode) {}
+
+    public static String PUSH_MESSAGE_URL = "https://api.pushover.net/1/messages.json";
+
+    public static final String REQUEST_ID_HEADER = "X-Request-Id";
+
+    private static final Logger log = LoggerFactory.getLogger(PushoverRestClient.class);
+
+    public void pushMessage(PushoverMessage msg) {
+
+        final var post = WebClient.create().post().uri(PUSH_MESSAGE_URL)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        final var valueMap = new LinkedMultiValueMap<String, String>();
+
+        valueMap.add("token", msg.getApiToken());
+        valueMap.add("user", msg.getUserId());
+        if (msg.getHtmlMessage() != null && !msg.getHtmlMessage().trim().isEmpty()) {
+            valueMap.add("message", msg.getHtmlMessage());
+        } else {
+            valueMap.add("message", msg.getMessage());
+        }
+
+        addPairIfNotNull(valueMap, "title", msg.getTitle());
+
+        addPairIfNotNull(valueMap, "url", msg.getUrl());
+        addPairIfNotNull(valueMap, "url_title", msg.getTitleForURL());
+
+        addPairIfNotNull(valueMap, "device", msg.getDevice());
+        addPairIfNotNull(valueMap, "timestamp", msg.getTimestamp());
+        addPairIfNotNull(valueMap, "sound", msg.getSound());
+
+        if (msg.getHtmlMessage() != null && !msg.getHtmlMessage().trim().isEmpty()) {
+            addPairIfNotNull(valueMap, "html", "1");
+        }
+
+        if (!MessagePriority.NORMAL.equals(msg.getPriority())) {
+            addPairIfNotNull(valueMap, "priority", msg.getPriority());
+        }
+
+        var ref = new Object() {
+            String requestId;
+        };
+        post.body(BodyInserters.fromFormData(valueMap)).exchangeToMono(response -> {
+            var statusCode = response.statusCode();
+            if (statusCode.equals(HttpStatus.OK)) {
+                ref.requestId = response.headers().header(REQUEST_ID_HEADER).get(0);
+                return response.bodyToMono(PushoverResponse.class);
+            }
+            return Mono.just(new PushoverErrorResponse(statusCode));
+        }).single().subscribe(result -> {
+            if (result.getClass().equals(PushoverResponse.class)) {
+                var r = (PushoverResponse) result;
+                log.info("Pushover notification response: status {}, requestId {}.",r.status, ref.requestId);
+            } else if (result.getClass().equals(PushoverErrorResponse.class)) {
+                var r = (PushoverErrorResponse) result;
+                log.error("Pushover request failed with status code {}.",r.statusCode.value());
+            }
+        });
+    }
+
+
+    private void addPairIfNotNull(MultiValueMap<String, String> multiValueMap, String key, Object value) {
+        if (value != null) {
+            multiValueMap.add(key, value.toString());
+        }
+    }
+
+    public static class PushoverResponse {
+        int status;
+    }
+}
